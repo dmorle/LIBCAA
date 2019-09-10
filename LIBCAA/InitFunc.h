282231a13ddac536dff4396a47a43c197d135974
dmorle <morled@uwindsor.ca>
2019-09-09 20:23:41 -0400

    added random initialization functions

--- LIBCAA/InitFunc.h
+++ LIBCAA/InitFunc.h
@@ -1,6 +1,8 @@
 #ifndef INITFUNC_H
 #define INITFUNC_H
 
+#include <random>
+
 namespace LIBCAA {
 
 	// initialization functions
@@ -12,6 +14,17 @@ namespace LIBCAA {
 			return val++;
 		}
 
+		// returns a random "dataType" between min and max distributed uniformally
+		template <typename dataType> dataType uniformDist(dataType min, dataType max) {
+			return (dataType)rand() / (dataType)(RAND_MAX / (max - min)) + min;
+		}
+
+		// returns a random "dataType" between min and max distributed binomially
+		template <typename dataType> dataType binomialDist(dataType min, dataType max) {
+			double res1 = uniformDist(((double)min) / 2, ((double)max) / 2);
+			double res2 = uniformDist(((double)min) / 2, ((double)max) / 2);
+			return (dataType)(res1 + res2);
+		}
 	}
 }
 
