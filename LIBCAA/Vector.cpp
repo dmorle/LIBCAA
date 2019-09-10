6932539827437df2e1d45b39fd27cd52a002dc9a
dmorle <morled@uwindsor.ca>
2019-09-10 12:00:52 -0400

    added Vector class

---
+++ LIBCAA/Vector.cpp
@@ -0,0 +1,24 @@
+#include "Vector.h"
+
+
+
+namespace LIBCAA {
+
+	Vector::Vector(int size, double val) : Tensor(1, &size, val)
+	{
+		this->type = "Vector";
+	}
+
+	Vector::Vector(int size, double *vals) : Tensor(1, &size, vals)
+	{
+		this->type = "Vector";
+	}
+
+	Vector::Vector(int size, double(*initFunc)()) : Tensor(1, &size, initFunc)
+	{
+		this->type = "Vector";
+	}
+
+	Vector::~Vector() {}
+
+}
