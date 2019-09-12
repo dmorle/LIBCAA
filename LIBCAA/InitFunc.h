403359ce948985a80b55c9855e070f42ee7f7831
dmorle <morled@uwindsor.ca>
2019-09-12 17:25:22 -0400

    initialization functions can now be passed to constructors

--- LIBCAA/InitFunc.h
+++ LIBCAA/InitFunc.h
@@ -15,15 +15,15 @@ namespace LIBCAA {
 		}
 
 		// returns a random "dataType" between min and max distributed uniformally
-		template <typename dataType> dataType uniformDist(dataType min, dataType max) {
-			return (dataType)rand() / (dataType)(RAND_MAX / (max - min)) + min;
+		template <typename dataType, int min, int max> dataType uniformDist() {
+			return ((double)rand() / (double)(RAND_MAX / (max - min))) + min;
 		}
 
 		// returns a random "dataType" between min and max distributed binomially
-		template <typename dataType> dataType binomialDist(dataType min, dataType max) {
-			double res1 = uniformDist(((double)min) / 2, ((double)max) / 2);
-			double res2 = uniformDist(((double)min) / 2, ((double)max) / 2);
-			return (dataType)(res1 + res2);
+		template <typename dataType, int min, int max> dataType binomialDist() {
+			double res1 = uniformDist<double, ((double)min) / 2, ((double)max) / 2>();
+			double res2 = uniformDist<double, ((double)min) / 2, ((double)max) / 2>();
+			return res1 + res2;
 		}
 	}
 }
