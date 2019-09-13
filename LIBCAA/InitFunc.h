e474ae9a0917c8ea3591b08450e6313500060960
dmorle <morled@uwindsor.ca>
2019-09-12 21:23:33 -0400

    added template overloads

--- LIBCAA/InitFunc.h
+++ LIBCAA/InitFunc.h
@@ -19,11 +19,23 @@ namespace LIBCAA {
 			return ((double)rand() / (double)(RAND_MAX / (max - min))) + min;
 		}
 
+		// returns a random "dataType" between min and max that was first cast to midType
+		template <typename dataType, typename midType, int min, int max> dataType uniformDist() {
+			return (midType)(((double)rand() / (double)(RAND_MAX / (max - min))) + min);
+		}
+
 		// returns a random "dataType" between min and max distributed binomially
 		template <typename dataType, int min, int max> dataType binomialDist() {
 			double res1 = uniformDist<double, ((double)min) / 2, ((double)max) / 2>();
 			double res2 = uniformDist<double, ((double)min) / 2, ((double)max) / 2>();
 			return res1 + res2;
+		}
+
+		// returns a random "dataType" between min and max that was first cast to midType
+		template <typename dataType, typename midType, int min, int max> dataType binomialDist() {
+			double res1 = uniformDist<double, ((double)min) / 2, ((double)max) / 2>();
+			double res2 = uniformDist<double, ((double)min) / 2, ((double)max) / 2>();
+			return (midType)(res1 + res2);
 		}
 	}
 }
