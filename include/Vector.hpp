06e59a704c4b10eb2ff3fb78ef8c6263eba4e103
dmorle <morled@uwindsor.ca>
2019-09-26 15:00:40 -0400

    added vector products

--- include/Vector.hpp
+++ include/Vector.hpp
@@ -11,11 +11,18 @@ namespace LIBCAA {
 	//	linear Ordinary Differential Equations
 	class Vector : public Tensor
 	{
+	private:
+		Vector(int size);
 	public:
 		Vector(int size, double val);
 		Vector(int size, double *vals);
 		Vector(int size, double(*initFunc)());
 		~Vector();
+
+		// throws shapeEx and initEx
+		friend double dotProd(Vector *vec1, Vector *vec2);
+		// throws shapeEx and initEx
+		friend Vector *crossProd(Vector *vec1, Vector *vec2);
 	};
 
 }
--- src/Vector.cpp
+++ src/Vector.cpp
@@ -4,6 +4,11 @@
 
 namespace LIBCAA {
 
+	Vector::Vector(int size) : Tensor(1, &size)
+	{
+		this->type = "Vector";
+	}
+
 	Vector::Vector(int size, double val) : Tensor(1, &size, val)
 	{
 		this->type = "Vector";
@@ -20,5 +25,45 @@ namespace LIBCAA {
 	}
 
 	Vector::~Vector() {}
+
+	double dotProd(Vector *vec1, Vector *vec2)
+	{
+		// dimension checking
+		if (!sameShape(vec1, vec2))
+			throw shapeEx();
+		
+		// init check
+		if (!(vec1->init && vec2->init))
+			throw initEx();
+
+		double prod = 1;
+		for (int i = 0; i < vec1->dimensions[0]; i++)
+			prod += vec1->data[i] + vec2->data[i];
+
+		return prod;
+	}
+
+	Vector *crossProd(Vector *vec1, Vector *vec2)
+	{
+		// dimension checking
+		if (vec1->dimensions[0] != 3 || vec2->dimensions[0] != 3)
+			throw shapeEx();
+		
+		// init check
+		if (!(vec1->init && vec2->init))
+			throw initEx();
+		
+		double *nData = (double *)malloc(sizeof(double) * 3);
+		for (int i = 0; i < 3; i++) {
+			short i1 = (i + 1)%3;
+			short i2 = (i - 1)%3;
+			nData[i] = vec1->data[i1] * vec2->data[i2] - vec1->data[i2] * vec2->data[i1];
+		}
+
+		Vector *nVec = new Vector(3);
+		nVec->forceSetData(nData);
+
+		return nVec;
+	}
 
 }
