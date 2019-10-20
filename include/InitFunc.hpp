3bd23e192e88987994eb0b10953a01f82088d146
dmorle <morled@uwindsor.ca>
2019-10-20 17:56:49 -0400

    started init restructure

--- include/InitFunc.hpp
+++ include/InitFunc.hpp
@@ -5,27 +5,6 @@
 
 namespace LIBCAA {
 
-	// initialization functions
-	namespace INIT {
-
-		// initializes sequential elements using the ++ operator
-		double arange();
-
-		// returns a random "dataType" between min and max that was first cast to midType
-		double uniformDist();
-
-		// returns a random "dataType" between min and max that was first cast to midType
-		double binomialDist();
-
-		// returns a sequence which creates a derivative matrix
-		double derivativeMatrix();
-
-		// returns a sequence which creates an identity matrix
-		double identityMatrix();
-
-		// returns a leviCivita tensor
-	}
-
 	// midType casts
 	enum DATA {
 		SHORT,
@@ -35,31 +14,40 @@ namespace LIBCAA {
 		DOUBLE
 	};
 
-	// initializes sequential elements using the ++ operator
-	double(*createArangeInit()) ();
+	class initFactory
+	{
+	public:
+		initFactory();
+		~initFactory();
 
-	double(*createArangeInit(int initVal)) ();
+		// initializes sequential elements using the ++ operator
+		double(*createArangeInit()) ();
 
-	// returns a random double between min and max distributed uniformally
-	double(*createUniformDist(double min, double max)) ();
+		double(*createArangeInit(int initVal)) ();
 
-	// returns a random double between min and max that was first cast to midType
-	double(*createUniformDist(double min, double max, DATA midType)) ();
+		// returns a random double between min and max distributed uniformally
+		double(*createUniformDist(double min, double max)) ();
 
-	// returns a random double between min and max distributed binomially
-	double(*createBinomialDist(double min, double max)) ();
+		// returns a random double between min and max that was first cast to midType
+		double(*createUniformDist(double min, double max, DATA midType)) ();
 
-	// returns a random double between min and max that was first cast to midType
-	double(*createBinomialDist(double min, double max, DATA midType)) ();
+		// returns a random double between min and max distributed binomially
+		double(*createBinomialDist(double min, double max)) ();
 
-	// returns the entries of a derivative matrix
-	double(*createDerivativeMatrix(int size)) ();
+		// returns a random double between min and max that was first cast to midType
+		double(*createBinomialDist(double min, double max, DATA midType)) ();
 
-	// returns the entries of an identity matrix
-	double(*createIdentityMatrix(int size)) ();
+		// returns the entries of a derivative matrix
+		double(*createDerivativeMatrix(int size)) ();
 
-	// creates a levi-civita tensor
-	double(*createLeviCivita(int rank)) ();
+		// returns the entries of an identity matrix
+		double(*createIdentityMatrix(int size)) ();
+
+		// creates a levi-civita tensor
+		double(*createLeviCivita(int rank)) ();
+
+	};
+
 }
 
 #endif
--- src/InitFunc.cpp
+++ src/InitFunc.cpp
@@ -6,72 +6,6 @@
 namespace LIBCAA {
     
     namespace INIT {
-        // variable for arange
-		int arangeVal;
-
-        // variable for random initializations
-        double min;
-        double max;
-        DATA midType;
-
-		// variables to keep track of indicies for an initialization
-		int rank;
-		int *dimensions;
-		int *index;
-
-        // currently only for matricies
-        void nextMatrixIndex() {
-			index[0] = (index[0] + 1) % dimensions[0];
-			if (!index[0] && ++index[1] == dimensions[1]) {
-				free(dimensions);
-				free(index);
-			}
-		}
-
-        // generalized for tensors
-        void nextIndex() {
-            int i = 0;
-            do {
-                index[i] = (index[i] + 1) % dimensions[i];
-            } while (!index[i++] && i < rank);
-            if (i == rank && !index[rank - 1]) {
-                free(dimensions);
-                free(index);
-            }
-        }
-
-        // determines whether the index is an even or odd parity (true => even)
-        bool findIndexParity() {
-            int cycleCount = 0;
-            for (int i = 0; i < rank; i++) {
-                if (index[i] < rank) {
-                    // start cycle
-                    cycleCount++;
-
-                    int tempI = i;
-                    do {
-                        /*for (int i = 0; i < rank; i++)
-                            std::cout<<index[i];
-                        std::cout<<"\tindex"<<std::endl;
-                        std::cin.get();
-
-                        printf("%d\ttempI", tempI);
-                        std::cin.get();*/
-
-                        int temp = index[tempI];
-                        index[tempI] += rank;
-                        tempI = temp;
-                    } while (tempI != i);
-                }
-            }
-
-            // unmarking the indicies
-            for (int i = 0; i < rank; i++)
-                index[i] -= rank;
-            
-            return (rank - cycleCount)%2;
-        }
-
 		// initializes sequential elements using the ++ operator
 		double arange() {
 			return arangeVal++;
@@ -158,59 +92,59 @@ namespace LIBCAA {
     // end of INIT namespace
 
     // initializes sequential elements using the ++ operator
-	double(*createArangeInit()) () {
+	double(*initFactory::createArangeInit()) () {
 		INIT::arangeVal = 0;
 
 		return INIT::arange;
 	}
 
-	double(*createArangeInit(int initVal)) () {
+	double(*initFactory::createArangeInit(int initVal)) () {
 		INIT::arangeVal = initVal;
 
 		return INIT::arange;
 	}
 
 	// returns a random double between min and max distributed uniformally
-	double(*createUniformDist(double min, double max)) () {
-        INIT::min = min;
-        INIT::max = max;
-        INIT::midType = DOUBLE;
+	double(*initFactory::createUniformDist(double min, double max)) () {
+        this->min = min;
+        this->max = max;
+        this->midType = DOUBLE;
 
 		return INIT::uniformDist;
 	}
 
     // returns a random double casted to midType and back
-	double(*createUniformDist(double min, double max, DATA midType)) () {
-        INIT::min = min;
-        INIT::max = max;
-        INIT::midType = midType;
+	double(*initFactory::createUniformDist(double min, double max, DATA midType)) () {
+        this->min = min;
+        this->max = max;
+        this->midType = midType;
 
 		return INIT::uniformDist;
 	}
 
     // returns a random double between min and max distributed binomially
-    double(*createBinomialDist(double min, double max)) () {
-        INIT::min = min;
-        INIT::max = max;
-        INIT::midType = DOUBLE;
+    double(*initFactory::createBinomialDist(double min, double max)) () {
+        this->min = min;
+        this->max = max;
+        this->midType = DOUBLE;
 
         return INIT::binomialDist;
     }
 
     // returns a random double between min and max that was first cast to midType
-    double(*createBinomialDist(double min, double max, DATA midType)) () {
-        INIT::min = min;
-        INIT::max = max;
-        INIT::midType = midType;
+    double(*initFactory::createBinomialDist(double min, double max, DATA midType)) () {
+        this->min = min;
+        this->max = max;
+        this->midType = midType;
 
         return INIT::binomialDist;
     }
 
     // returns the entries of a derivative matrix
-    double(*createDerivativeMatrix(int size)) () {
-		INIT::rank = 2;
-		INIT::dimensions = (int *)malloc(sizeof(int) * 2);
-		INIT::index = (int *)malloc(sizeof(int) * 2);
+    double(*initFactory::createDerivativeMatrix(int size)) () {
+		this->rank = 2;
+		this->dimensions = (int *)malloc(sizeof(int) * 2);
+		this->index = (int *)malloc(sizeof(int) * 2);
 
 		INIT::dimensions[0] = INIT::dimensions[1] = size;
 		INIT::index[0] = INIT::index[1] = 0;
@@ -219,7 +153,7 @@ namespace LIBCAA {
 	}
 
     // returns the entries of an identity matrix
-	double(*createIdentityMatrix(int size)) () {
+	double(*initFactory::createIdentityMatrix(int size)) () {
 		INIT::dimensions = (int *)malloc(sizeof(int) * 2);
 		INIT::index = (int *)malloc(sizeof(int) * 2);
 
@@ -230,7 +164,7 @@ namespace LIBCAA {
 	}
 
     // creates a levi-civita tensor
-    double(*createLeviCivita(int rank)) () {
+    double(*initFactory::createLeviCivita(int rank)) () {
         INIT::rank = rank;
         INIT::dimensions = (int *)malloc(sizeof(int) * rank);
         INIT::index = (int *)malloc(sizeof(int) * rank);
@@ -242,4 +176,89 @@ namespace LIBCAA {
 
         return INIT::leviCivita;
     }
+
+    class init
+	{
+	public:
+		init();
+		~init();
+
+		// initializes sequential elements using the ++ operator
+		double arange();
+
+		// returns a random "dataType" between min and max that was first cast to midType
+		double uniformDist();
+
+		// returns a random "dataType" between min and max that was first cast to midType
+		double binomialDist();
+
+		// returns a sequence which creates a derivative matrix
+		double derivativeMatrix();
+
+		// returns a sequence which creates an identity matrix
+		double identityMatrix();
+
+		// returns a leviCivita tensor
+		double leviCivita();
+
+	private:
+		// variable for arange
+		int arangeVal;
+
+        // variable for random initializations
+        double min;
+        double max;
+        DATA midType;
+
+		// variables to keep track of indicies for an initialization
+		int rank;
+		int *dimensions;
+		int *index;
+
+		// currently only for matricies
+        void nextMatrixIndex() {
+			index[0] = (index[0] + 1) % dimensions[0];
+			if (!index[0] && ++index[1] == dimensions[1]) {
+				free(dimensions);
+				free(index);
+			}
+		}
+
+        // generalized for tensors
+        void nextIndex() {
+            int i = 0;
+            do {
+                index[i] = (index[i] + 1) % dimensions[i];
+            } while (!index[i++] && i < rank);
+            if (i == rank && !index[rank - 1]) {
+                free(dimensions);
+                free(index);
+            }
+        }
+
+        // determines whether the index is an even or odd parity (true => even)
+        bool findIndexParity() {
+            int cycleCount = 0;
+            for (int i = 0; i < rank; i++) {
+                if (index[i] < rank) {
+                    // start cycle
+                    cycleCount++;
+
+                    int tempI = i;
+                    do {
+                        int temp = index[tempI];
+                        index[tempI] += rank;
+                        tempI = temp;
+                    } while (tempI != i);
+                }
+            }
+
+            // unmarking the indicies
+            for (int i = 0; i < rank; i++)
+                index[i] -= rank;
+            
+            return (rank - cycleCount)%2;
+        }
+	};
+
 }
