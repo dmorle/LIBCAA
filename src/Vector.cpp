d10db10cc19994a592ec592a2f84487564ba65b0
dmorle <morled@uwindsor.ca>
2019-10-21 17:41:38 -0400

    implimented non-comparison operators

--- src/Vector.cpp
+++ src/Vector.cpp
@@ -46,154 +46,350 @@ namespace LIBCAA {
 		return getAbsIndex(i % this->len);
 	}
 
-	Vector *Vector::operator+(Vector *vec) {
-		this->opExcept(vec);
+	Vector *add(Vector *vec1, Vector *vec2)
+	{
+		vec1->opExcept(vec2);
 
-		double *nData = (double *)malloc(sizeof(double) * this->len);
-		for (int i = 0; i < this->len; i++)
-			nData[i] = this->data[i] + vec->getDataIndex(i);
+		double *nData = (double *)malloc(sizeof(double) * vec1->len);
+		for (int i = 0; i < vec1->len; i++)
+			nData[i] = vec1->data[i] + vec2->data[i];
 
-		Vector *npVec = new Vector(this->len);
+		Vector *npVec = new Vector(vec1->len);
 		npVec->forceSetData(nData);
 
 		return npVec;
 	}
 
-	Vector *Vector::operator+(double val) {
-		if (!this->init)
-			throw initEx();
+	Vector *add(Vector *vec, double val)
+	{
+		double *nData = (double *)malloc(sizeof(double) * vec->len);
+		for (int i = 0; i < vec->len; i++)
+			nData[i] = vec->data[i] + val;
 
-		double *nData = (double *)malloc(sizeof(double) * this->len);
-		for (int i = 0; i < this->len; i++)
-			nData[i] = this->data[i] + val;
-
-		Vector *npVec = new Vector(this->len);
+		Vector *npVec = new Vector(vec->len);
 		npVec->forceSetData(nData);
 
 		return npVec;
 	}
 
-	Vector *Vector::operator-(Vector *vec) {
-		this->opExcept(vec);
+	Vector *add(double val, Vector *vec)
+	{
+		double *nData = (double *)malloc(sizeof(double) * vec->len);
+		for (int i = 0; i < vec->len; i++)
+			nData[i] = val + vec->data[i];
 
-		double *nData = (double *)malloc(sizeof(double) * this->len);
-		for (int i = 0; i < this->len; i++)
-			nData[i] = this->data[i] - vec->getDataIndex(i);
+		Vector *npVec = new Vector(vec->len);
+		npVec->forceSetData(nData);
 
-		Vector *npVec = new Vector(this->len);
+		return npVec;
+	}
+	
+	Vector *sub(Vector *vec1, Vector *vec2)
+	{
+		vec1->opExcept(vec2);
+
+		double *nData = (double *)malloc(sizeof(double) * vec1->len);
+		for (int i = 0; i < vec1->len; i++)
+			nData[i] = vec1->data[i] - vec2->data[i];
+
+		Vector *npVec = new Vector(vec1->len);
 		npVec->forceSetData(nData);
 
 		return npVec;
 	}
 
-	Vector *Vector::operator-(double val) {
-		if (!this->init)
-			throw initEx();
+	Vector *sub(Vector *vec, double val)
+	{
+		double *nData = (double *)malloc(sizeof(double) * vec->len);
+		for (int i = 0; i < vec->len; i++)
+			nData[i] = vec->data[i] - val;
 
-		double *nData = (double *)malloc(sizeof(double) * this->len);
-		for (int i = 0; i < this->len; i++)
-			nData[i] = this->data[i] - val;
-
-		Vector *npVec = new Vector(this->len);
+		Vector *npVec = new Vector(vec->len);
 		npVec->forceSetData(nData);
 
 		return npVec;
 	}
 
-	Vector *Vector::operator*(Vector *vec) {
-		this->opExcept(vec);
+	Vector *sub(double val, Vector *vec)
+	{
+		double *nData = (double *)malloc(sizeof(double) * vec->len);
+		for (int i = 0; i < vec->len; i++)
+			nData[i] = val - vec->data[i];
 
-		double *nData = (double *)malloc(sizeof(double) * this->len);
-		for (int i = 0; i < this->len; i++)
-			nData[i] = this->data[i] * vec->getDataIndex(i);
-
-		Vector *npVec = new Vector(this->len);
+		Vector *npVec = new Vector(vec->len);
 		npVec->forceSetData(nData);
 
 		return npVec;
 	}
 
-	Vector *Vector::operator*(double val) {
-		if (!this->init)
-			throw initEx();
+	Vector *mul(Vector *vec1, Vector *vec2)
+	{
+		vec1->opExcept(vec2);
 
-		double *nData = (double *)malloc(sizeof(double) * this->len);
-		for (int i = 0; i < this->len; i++)
-			nData[i] = this->data[i] * val;
+		double *nData = (double *)malloc(sizeof(double) * vec1->len);
+		for (int i = 0; i < vec1->len; i++)
+			nData[i] = vec1->data[i] * vec2->data[i];
 
-		Vector *npVec = new Vector(this->len);
+		Vector *npVec = new Vector(vec1->len);
 		npVec->forceSetData(nData);
 
 		return npVec;
 	}
 
-	Vector *Vector::operator/(Vector *vec) {
-		this->opExcept(vec);
+	Vector *mul(Vector *vec, double val)
+	{
+		double *nData = (double *)malloc(sizeof(double) * vec->len);
+		for (int i = 0; i < vec->len; i++)
+			nData[i] = vec->data[i] * val;
 
-		double *nData = (double *)malloc(sizeof(double) * this->len);
-		for (int i = 0; i < this->len; i++)
-			nData[i] = this->data[i] / vec->getDataIndex(i);
-
-		Vector *npVec = new Vector(this->len);
+		Vector *npVec = new Vector(vec->len);
 		npVec->forceSetData(nData);
 
 		return npVec;
 	}
 
-	Vector *Vector::operator/(double val) {
-		if (!this->init)
-			throw initEx();
+	Vector *mul(double val, Vector *vec)
+	{
+		double *nData = (double *)malloc(sizeof(double) * vec->len);
+		for (int i = 0; i < vec->len; i++)
+			nData[i] = val * vec->data[i];
 
-		double *nData = (double *)malloc(sizeof(double) * this->len);
-		for (int i = 0; i < this->len; i++)
-			nData[i] = this->data[i] / val;
-
-		Vector *npVec = new Vector(this->len);
+		Vector *npVec = new Vector(vec->len);
 		npVec->forceSetData(nData);
 
 		return npVec;
 	}
 
-	Vector *Vector::operator^(Vector *vec) {
-		this->opExcept(vec);
+	Vector *div(Vector *vec1, Vector *vec2)
+	{
+		vec1->opExcept(vec2);
 
-		double *nData = (double *)malloc(sizeof(double) * this->len);
-		for (int i = 0; i < this->len; i++)
-			nData[i] = std::pow(this->data[i], vec->getDataIndex(i));
+		double *nData = (double *)malloc(sizeof(double) * vec1->len);
+		for (int i = 0; i < vec1->len; i++)
+			nData[i] = vec1->data[i] / vec2->data[i];
 
-		Vector *npVec = new Vector(this->len);
-		npVec->forceSetData(nData);
-
-
-		return npVec;
-	}
-
-	Vector *Vector::operator^(double val) {
-		if (!this->init)
-			throw initEx();
-
-		double *nData = (double *)malloc(sizeof(double) * this->len);
-		for (int i = 0; i < this->len; i++)
-			nData[i] = std::pow(this->data[i], val);
-
-		Vector *npVec = new Vector(this->len);
+		Vector *npVec = new Vector(vec1->len);
 		npVec->forceSetData(nData);
 
 		return npVec;
 	}
 
-	Vector *Vector::operator^(int val) {
-		if (!this->init)
-			throw initEx();
+	Vector *div(Vector *vec, double val)
+	{
+		double *nData = (double *)malloc(sizeof(double) * vec->len);
+		for (int i = 0; i < vec->len; i++)
+			nData[i] = vec->data[i] / val;
 
-		double *nData = (double *)malloc(sizeof(double) * this->len);
-		for (int i = 0; i < this->len; i++)
-			nData[i] = std::pow(this->data[i], val);
-
-		Vector *npVec = new Vector(this->len);
+		Vector *npVec = new Vector(vec->len);
 		npVec->forceSetData(nData);
 
 		return npVec;
+	}
+
+	Vector *div(double val, Vector *vec)
+	{
+		double *nData = (double *)malloc(sizeof(double) * vec->len);
+		for (int i = 0; i < vec->len; i++)
+			nData[i] = val / vec->data[i];
+
+		Vector *npVec = new Vector(vec->len);
+		npVec->forceSetData(nData);
+
+		return npVec;
+	}
+
+	Vector *pow(Vector *vec1, Vector *vec2)
+	{
+		vec1->opExcept(vec2);
+
+		double *nData = (double *)malloc(sizeof(double) * vec1->len);
+		for (int i = 0; i < vec1->len; i++)
+			nData[i] = std::pow(vec1->data[i], vec2->data[i]);
+
+		Vector *npVec = new Vector(vec1->len);
+		npVec->forceSetData(nData);
+
+		return npVec;
+	}
+
+	Vector *pow(Vector *vec, double val)
+	{
+		double *nData = (double *)malloc(sizeof(double) * vec->len);
+		for (int i = 0; i < vec->len; i++)
+			nData[i] = std::pow(vec->data[i], val);
+
+		Vector *npVec = new Vector(vec->len);
+		npVec->forceSetData(nData);
+
+		return npVec;
+	}
+
+	Vector *pow(double val, Vector *vec)
+	{
+		double *nData = (double *)malloc(sizeof(double) * vec->len);
+		for (int i = 0; i < vec->len; i++)
+			nData[i] = std::pow(val, vec->data[i]);
+
+		Vector *npVec = new Vector(vec->len);
+		npVec->forceSetData(nData);
+
+		return npVec;
+	}
+
+	Vector *pow(Vector *vec, int val)
+	{
+		double *nData = (double *)malloc(sizeof(double) * vec->len);
+		for (int i = 0; i < vec->len; i++)
+			nData[i] = std::pow(vec->data[i], val);
+
+		Vector *npVec = new Vector(vec->len);
+		npVec->forceSetData(nData);
+
+		return npVec;
+	}
+
+
+	Vector *addAcc(int len, Vector **vecArr)
+	{
+		for (int i = 1; i < len; i++)
+			vecArr[0]->opExcept(vecArr[i]);
+
+		double *nData = (double *)calloc(vecArr[0]->len, sizeof(double));
+
+		for (int i = 0; i < vecArr[0]->len; i++)
+			for (int j = 0; j < len; j++)
+				nData[j] += vecArr[j]->data[i];
+		
+		Vector *npVec = new Vector(vecArr[0]->len);
+		npVec->forceSetData(nData);
+
+		return npVec;
+	}
+
+	Vector *mulAcc(int len, Vector **vecArr)
+	{
+		for (int i = 1; i < len; i++)
+			vecArr[0]->opExcept(vecArr[i]);
+
+		double *nData = (double *)malloc(sizeof(double) * vecArr[0]->len);
+		for (int j = 0; j < vecArr[0]->len; j++)
+			nData[j] = vecArr[0]->data[j];
+
+		for (int i = 1; i < vecArr[0]->len; i++)
+			for (int j = 0; j < len; j++)
+				nData[j] *= vecArr[j]->data[i];
+		
+		Vector *npVec = new Vector(vecArr[0]->len);
+		npVec->forceSetData(nData);
+
+		return npVec;
+	}
+	
+
+	void iadd(Vector *vec1, Vector *vec2)
+	{
+		vec1->opExcept(vec2);
+
+		for (int i = 0; i < vec1->len; i++)
+			vec1->data[i] += vec2->data[i];
+	}
+
+	void iadd(Vector *vec, double val)
+	{
+		for (int i = 0; i < vec->len; i++)
+			vec->data[i] += val;
+	}
+
+	void iadd(double val, Vector *vec)
+	{
+		for (int i = 0; i < vec->len; i++)
+			vec->data[i] += val;
+	}
+
+	void isub(Vector *vec1, Vector *vec2)
+	{
+		vec1->opExcept(vec2);
+
+		for (int i = 0; i < vec1->len; i++)
+			vec1->data[i] -= vec2->data[i];
+	}
+
+	void isub(Vector *vec, double val)
+	{
+		for (int i = 0; i < vec->len; i++)
+			vec->data[i] -= val;
+	}
+
+	void isub(double val, Vector *vec)
+	{
+		for (int i = 0; i < vec->len; i++)
+			vec->data[i] = val - vec->data[i];
+	}
+
+	void imul(Vector *vec1, Vector *vec2)
+	{
+		vec1->opExcept(vec2);
+
+		for (int i = 0; i < vec1->len; i++)
+			vec1->data[i] *= vec2->data[i];
+	}
+
+	void imul(Vector *vec, double val)
+	{
+		for (int i = 0; i < vec->len; i++)
+			vec->data[i] *= val;
+	}
+
+	void imul(double val, Vector *vec)
+	{
+		for (int i = 0; i < vec->len; i++)
+			vec->data[i] *= val;
+	}
+
+	void idiv(Vector *vec1, Vector *vec2)
+	{
+		vec1->opExcept(vec2);
+
+		for (int i = 0; i < vec1->len; i++)
+			vec1->data[i] /= vec2->data[i];
+	}
+
+	void idiv(Vector *vec, double val)
+	{
+		for (int i = 0; i < vec->len; i++)
+			vec->data[i] /= vec->data[i];
+	}
+
+	void idiv(double val, Vector *vec)
+	{
+		for (int i = 0; i < vec->len; i++)
+			vec->data[i] = val/vec->data[i];
+	}
+
+	void ipow(Vector *vec1, Vector *vec2)
+	{
+		vec1->opExcept(vec2);
+
+		for (int i = 0; i < vec1->len; i++)
+			vec1->data[i] = std::pow(vec1->data[i], vec2->data[i]);
+	}
+
+	void ipow(Vector *vec, double val)
+	{
+		for (int i = 0; i < vec->len; i++)
+			vec->data[i] = std::pow(vec->data[i], val);
+	}
+
+	void ipow(double val, Vector *vec)
+	{
+		for (int i = 0; i , vec->len; i++)
+			vec->data[i] = std::pow(val, vec->data[i]);
+	}
+
+	void ipow(Vector *vec, int val)
+	{
+		for (int i = 0; i < vec->len; i++)
+			vec->data[i] = std::pow(vec->data[i], val);
 	}
 
 	double dotProd(Vector *vec1, Vector *vec2)
