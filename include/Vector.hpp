f4172e31c41e33724dd6ddf87b651a0b660a1622
dmorle <morled@uwindsor.ca>
2019-10-21 17:30:56 -0400

    added function declarations for Vector operations

--- include/Vector.hpp
+++ include/Vector.hpp
@@ -1,7 +1,7 @@
 #ifndef VECTOR_H
 #define VECTOR_H
 
-#include "Matrix.hpp"
+#include "Vector.hpp"
 
 namespace LIBCAA {
 
@@ -20,17 +20,49 @@ namespace LIBCAA {
 
 		~Vector();
 
-		Vector *operator + (Vector *vec);
-		Vector *operator + (double val);
-		Vector *operator - (Vector *vec);
-		Vector *operator - (double val);
-		Vector *operator * (Vector *vec);
-		Vector *operator * (double val);
-		Vector *operator / (Vector *vec);
-		Vector *operator / (double val);
-		Vector *operator ^ (Vector *vec);
-		Vector *operator ^ (double val);
-		Vector *operator ^ (int val);
+		friend Vector *add(Vector *vec1, Vector *vec2);
+		friend Vector *add(Vector *vec, double val);
+		friend Vector *add(double val, Vector *vec);
+		friend Vector *sub(Vector *vec1, Vector *vec2);
+		friend Vector *sub(Vector *vec, double val);
+		friend Vector *sub(double val, Vector *vec);
+		friend Vector *mul(Vector *vec1, Vector *vec2);
+		friend Vector *mul(Vector *vec, double val);
+		friend Vector *mul(double val, Vector *vec);
+		friend Vector *div(Vector *vec1, Vector *vec2);
+		friend Vector *div(Vector *vec, double val);
+		friend Vector *div(double val, Vector *vec);
+		friend Vector *pow(Vector *vec1, Vector *vec2);
+		friend Vector *pow(Vector *vec, double val);
+		friend Vector *pow(double val, Vector *vec);
+		friend Vector *pow(Vector *vec, int val);
+
+		friend Vector *addAcc(int len, Vector **mrxArr);
+		friend Vector *mulAcc(int len, Vector **mrxArr);
+
+		friend void iadd(Vector *vec1, Vector *vec2);
+		friend void iadd(Vector *vec, double val);
+		friend void iadd(double val, Vector *vec);
+		friend void isub(Vector *vec1, Vector *vec2);
+		friend void isub(Vector *vec, double val);
+		friend void isub(double val, Vector *vec);
+		friend void imul(Vector *vec1, Vector *vec2);
+		friend void imul(Vector *vec, double val);
+		friend void imul(double val, Vector *vec);
+		friend void idiv(Vector *vec1, Vector *vec2);
+		friend void idiv(Vector *vec, double val);
+		friend void idiv(double val, Vector *vec);
+		friend void ipow(Vector *vec1, Vector *vec2);
+		friend void ipow(Vector *vec, double val);
+		friend void ipow(double val, Vector *vec);
+		friend void ipow(Vector *vec, int val);
+
+		friend Vector *eq(Vector *vec);
+		friend Vector *ne(Vector *vec);
+		friend Vector *gt(Vector *vec);
+		friend Vector *lt(Vector *vec);
+		friend Vector *le(Vector *vec);
+		friend Vector *ge(Vector *vec);
 
 		// overriding Tensor functions for efficiency
 		void setAbsIndex(int i, double val);
