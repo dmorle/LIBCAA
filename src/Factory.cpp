9ee6aab9a93a432e66166d5a0f53f1e3b56639a9
dmorle <morled@uwindsor.ca>
2019-10-29 16:35:09 -0400

    bug fixes

--- src/Factory.cpp
+++ src/Factory.cpp
@@ -203,9 +203,9 @@ namespace LIBCAA {
         for (int i = 0; i < size; i++) {
             for (int j = 0; j < size; j++) {
                 if (i == j)
-                    npMat->data[i * size + j] = 1;
+                    data[i * size + j] = 1;
                 else
-                    npMat->data[i * size + j] = 0;
+                    data[i * size + j] = 0;
             }
         }
 
