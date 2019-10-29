b11df8110efa3eab3d835d61642816c681d73cbe
dmorle <morled@uwindsor.ca>
2019-10-29 15:13:06 -0400

    set objList to be char **

--- include/Factory.hpp
+++ include/Factory.hpp
@@ -4,9 +4,6 @@
 #include <random>
 #include "Vector.hpp"
 #include "Matrix.hpp"
-#include "Tensor.hpp"
-
-#define OBJ_TOTAL
 
 namespace LIBCAA {
 
@@ -14,40 +11,39 @@ namespace LIBCAA {
 	{
 	public:
 
-		static Tensor *noInit(int rank, int *dimensions);
-		static Matrix *noInit(int m, int n);
-		static Vector *noInit(int len);
+		Tensor *noInit(int rank, int *dimensions);
+		Matrix *noInit(int m, int n);
+		Vector *noInit(int len);
 
-		static Tensor *constant(int rank, int *dimensions, double val=0);
-		static Matrix *constant(int m, int n, double val=0);
-		static Vector *constant(int len, double val=0);
+		Tensor *constant(int rank, int *dimensions, double val=0);
+		Matrix *constant(int m, int n, double val=0);
+		Vector *constant(int len, double val=0);
 
-		static Tensor *arange(int rank, int *dimensions, double start=0);
-		static Matrix *arange(int m, int n, double start=0);
-		static Vector *arange(int len, double start=0);
+		Tensor *arange(int rank, int *dimensions, double start=0);
+		Matrix *arange(int m, int n, double start=0);
+		Vector *arange(int len, double start=0);
 
-		static Tensor *uniDist(int rank, int *dimensions, double min=0, double max=1);
-		static Matrix *uniDist(int m, int n, double min=0, double max=1);
-		static Vector *uniDist(int len, double min=0, double max=1);
+		Tensor *uniDist(int rank, int *dimensions, double min=0, double max=1);
+		Matrix *uniDist(int m, int n, double min=0, double max=1);
+		Vector *uniDist(int len, double min=0, double max=1);
 
-		static Tensor *normDist(int rank, int *dimensions, double mean=0, double stdDev=1);
-		static Matrix *normDist(int m, int n, double mean=0, double stdDev=1);
-		static Vector *normDist(int len, double mean=0, double stdDev=1);
+		Tensor *normDist(int rank, int *dimensions, double mean=0, double stdDev=1);
+		Matrix *normDist(int m, int n, double mean=0, double stdDev=1);
+		Vector *normDist(int len, double mean=0, double stdDev=1);
 
-		static Tensor *leviCivita(int rank, int *dimensions);
+		Tensor *leviCivita(int rank, int *dimensions);
 
-		static Matrix *identity(int size);
-		static Matrix *derivativeMatrix(int size);
+		Matrix *identity(int size);
+		Matrix *derivativeMatrix(int size);
 
 		// used for deleting LIBCAA objects
-		static void release(void *_obj);
+		void release(void *_obj);
 	
 		// List of all non-virtual objects
-		const static std::string objList[OBJ_TOTAL];
+		char objList[4][64]{ "MDA", "Tensor", "Matrix", "Vector" };
+
 	};
-
-	const std::string Factory::objList[OBJ_TOTAL] = { "MDA", "Tensor", "Matrix", "Vector" };
-
+	
 }
 
 #endif
