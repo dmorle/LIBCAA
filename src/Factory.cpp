736671d460565ab139265d9078df578559d3081f
dmorle <morled@uwindsor.ca>
2019-10-29 16:20:13 -0400

    added identity initializaton

--- src/Factory.cpp
+++ src/Factory.cpp
@@ -200,7 +200,14 @@ namespace LIBCAA {
 
         double *data = (double *)malloc(sizeof(double) * npMat->len);
 
-        // TODO: initialize data
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                if (i == j)
+                    npMat->data[i * size + j] = 1;
+                else
+                    npMat->data[i * size + j] = 0;
+            }
+        }
 
         npMat->forceSetData(data);
         return npMat;
