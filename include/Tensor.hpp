db38f54aed81cef01ddba7d20e6ac533471ebdcd
dmorle <morled@uwindsor.ca>
2019-10-29 16:13:04 -0400

    easier access to operations

--- include/Tensor.hpp
+++ include/Tensor.hpp
@@ -84,6 +84,59 @@ namespace LIBCAA {
 		void opExcept(Tensor *pTens, bool checkType);
 	};
 
+	Tensor *add(Tensor *tens1, Tensor *tens2);
+	Tensor *add(Tensor *tens, double val);
+	Tensor *add(double val, Tensor *tens);
+	Tensor *sub(Tensor *tens1, Tensor *tens2);
+	Tensor *sub(Tensor *tens, double val);
+	Tensor *sub(double val, Tensor *tens);
+	Tensor *mul(Tensor *tens1, Tensor *tens2);
+	Tensor *mul(Tensor *tens, double val);
+	Tensor *mul(double val, Tensor *tens);
+	Tensor *div(Tensor *tens1, Tensor *tens2);
+	Tensor *div(Tensor *tens, double val);
+	Tensor *div(double val, Tensor *tens);
+	Tensor *pow(Tensor *tens1, Tensor *tens2);
+	Tensor *pow(Tensor *tens, double val);
+	Tensor *pow(double val, Tensor *tens);
+	Tensor *pow(Tensor *tens, int val);
+
+	Tensor *addAcc(int len, Tensor **tensArr);
+	Tensor *mulAcc(int len, Tensor **tensArr);
+
+	void iadd(Tensor *tens1, Tensor *tens2);
+	void iadd(Tensor *tens, double val);
+	void iadd(double val, Tensor *tens);
+	void isub(Tensor *tens1, Tensor *tens2);
+	void isub(Tensor *tens, double val);
+	void isub(double val, Tensor *tens);
+	void imul(Tensor *tens1, Tensor *tens2);
+	void imul(Tensor *tens, double val);
+	void imul(double val, Tensor *tens);
+	void idiv(Tensor *tens1, Tensor *tens2);
+	void idiv(Tensor *tens, double val);
+	void idiv(double val, Tensor *tens);
+	void ipow(Tensor *tens1, Tensor *tens2);
+	void ipow(Tensor *tens, double val);
+	void ipow(double val, Tensor *tens);
+	void ipow(Tensor *tens, int val);
+
+	Tensor *eq(Tensor *tens);
+	Tensor *ne(Tensor *tens);
+	Tensor *gt(Tensor *tens);
+	Tensor *lt(Tensor *tens);
+	Tensor *le(Tensor *tens);
+	Tensor *ge(Tensor *tens);
+
+	Tensor *transpose(Tensor *tens, int *axisOrder);
+	Tensor *collapseAxis(Tensor *tens, int axisNum, int *axes);
+
+	Tensor *applyFunc(Tensor *tens, double(*func)(double));
+
+	Tensor *outerProd(Tensor *tens1, Tensor *tens2);
+	double innerProd(Tensor *tens1, Tensor *tens2);
+	Tensor *tensorDot(Tensor *tens1, Tensor *tens2, int axisNum, int **axisPairs);
+
 }
 
 #endif // !TENSOR_H
--- include/Vector.hpp
+++ include/Vector.hpp
@@ -17,6 +17,12 @@ namespace LIBCAA {
 
 		Vector *clone();
 
+		// overriding Tensor functions for efficiency
+		void setAbsIndex(int i, double val);
+		void setIndex(int i, double val);
+		double getAbsIndex(int i);
+		double getIndex(int i);
+
 		friend Vector *add(Vector *vec1, Vector *vec2);
 		friend Vector *add(Vector *vec, double val);
 		friend Vector *add(double val, Vector *vec);
@@ -61,12 +67,6 @@ namespace LIBCAA {
 		friend Vector *le(Vector *vec);
 		friend Vector *ge(Vector *vec);
 
-		// overriding Tensor functions for efficiency
-		void setAbsIndex(int i, double val);
-		void setIndex(int i, double val);
-		double getAbsIndex(int i);
-		double getIndex(int i);
-
 		// throws shapeEx and initEx
 		friend double dotProd(Vector *vec1, Vector *vec2);
 		// throws shapeEx and initEx
@@ -83,6 +83,57 @@ namespace LIBCAA {
 		~Vector();
 	};
 
+
+	Vector *add(Vector *vec1, Vector *vec2);
+	Vector *add(Vector *vec, double val);
+	Vector *add(double val, Vector *vec);
+	Vector *sub(Vector *vec1, Vector *vec2);
+	Vector *sub(Vector *vec, double val);
+	Vector *sub(double val, Vector *vec);
+	Vector *mul(Vector *vec1, Vector *vec2);
+	Vector *mul(Vector *vec, double val);
+	Vector *mul(double val, Vector *vec);
+	Vector *div(Vector *vec1, Vector *vec2);
+	Vector *div(Vector *vec, double val);
+	Vector *div(double val, Vector *vec);
+	Vector *pow(Vector *vec1, Vector *vec2);
+	Vector *pow(Vector *vec, double val);
+	Vector *pow(double val, Vector *vec);
+	Vector *pow(Vector *vec, int val);
+
+	Vector *addAcc(int len, Vector **mrxArr);
+	Vector *mulAcc(int len, Vector **mrxArr);
+
+	void iadd(Vector *vec1, Vector *vec2);
+	void iadd(Vector *vec, double val);
+	void iadd(double val, Vector *vec);
+	void isub(Vector *vec1, Vector *vec2);
+	void isub(Vector *vec, double val);
+	void isub(double val, Vector *vec);
+	void imul(Vector *vec1, Vector *vec2);
+	void imul(Vector *vec, double val);
+	void imul(double val, Vector *vec);
+	void idiv(Vector *vec1, Vector *vec2);
+	void idiv(Vector *vec, double val);
+	void idiv(double val, Vector *vec);
+	void ipow(Vector *vec1, Vector *vec2);
+	void ipow(Vector *vec, double val);
+	void ipow(double val, Vector *vec);
+	void ipow(Vector *vec, int val);
+
+	Vector *eq(Vector *vec);
+	Vector *ne(Vector *vec);
+	Vector *gt(Vector *vec);
+	Vector *lt(Vector *vec);
+	Vector *le(Vector *vec);
+	Vector *ge(Vector *vec);
+
+	// throws shapeEx and initEx
+	double dotProd(Vector *vec1, Vector *vec2);
+	// throws shapeEx and initEx
+	Vector *crossProd(Vector *vec1, Vector *vec2);
+
+	Vector *matmul(Matrix *mrx, Vector *vec);
 }
 
 #endif
